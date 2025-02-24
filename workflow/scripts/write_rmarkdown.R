70883326ed7fb63e725ecda3fdaa86441630656f
jasperverwilt <jasper.verwilt@gmail.com>
2025-02-24 14:05:36 +0100

    add QC rmarkdown

---
+++ workflow/scripts/write_rmarkdown.R
@@ -0,0 +1,5 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+library(rmarkdown)
+rmarkdown::render('workflow/scripts/QC.Rmd',params=list(args =  args[1]),  output_file = args[2])
