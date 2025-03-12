dd0fa01214c8d62d3f7f3f28bdb20e833e8d17ef
jasperverwilt <jasper.verwilt@gmail.com>
2025-03-12 14:32:03 +0100

    add additional QC metrics

--- workflow/scripts/write_rmarkdown.R
+++ workflow/scripts/write_rmarkdown.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
+
 library(rmarkdown)
 rmarkdown::render('workflow/scripts/QC.Rmd',params=list(args =  args[1]),  output_file = args[2])
